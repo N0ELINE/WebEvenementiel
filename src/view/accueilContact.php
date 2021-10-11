042e0acac3177552d91a449705bf1ecf38240b33
athenaisT <athenais.thiburce@gmail.com>
2021-10-11 17:29:20 +0200

    contact maps

--- src/view/accueilContact.php
+++ src/view/accueilContact.php
@@ -14,26 +14,42 @@
     <form action="" id="form_contact">
         
             
-        <h1 id="title_contact">Nous contacter !</h1> <br>
+        <h1 id="title_contact">Nous contacter !</h1> <br><br><br><br> <br> <br> <br> <br> <br> 
             <div class="contact1">
                 <input type="text" id="name" name="name" value="Name" class="contact"required>
-                <input type="text" id="societe" name="societe" value="société" class="contact"required>
-            </div> <br>
+                <input type="text" id="societe" name="societe" value="société" class="contact"required> <br> <br> <br>
+            </div> <br> <br>
 
             <div>
-                <input type="email" id="email" name="email" value="email" class="contact" required>
-                <input type="tel" id="tel" name="tel" value="telephone" class="contact" required>
+                <input type="email" id="email" name="email" value="email" class="contact" required> 
+                <input type="tel" id="tel" name="tel" value="telephone" class="contact" required> <br> <br> <br>
             </div> <br>
-            <br>
+            <br> <br>
 
-            <input type="text" id="contact" name="sujet" value="sujet" class="contact" required> <br><br><br>
-            <textarea id="contact" name="corp" rows="5" cols="33" class="contact"></textarea><br>
-            <button id="envoyer_btn">Envoyer</button>      
+            <div> 
+            <input type="text" id="contact" name="sujet" value="sujet" class="contact_suj" required> <br><br> 
+            </div>  <br><br>  <br><br> 
+            
+            <div><textarea id="contact" name="corp" rows="5" cols="33" class="contact_corp"></textarea></div><br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+            <div class="buttons">
+            <button class="btn-hover color-3">Envoyer</button>
+            </div>          
+    </form> <br><br><br>
+   
 
-        
-    </form>
-
-
+    <div class="parent">
+            <div class="div1">
+                <img src="../image/bus.png" alt="bus" id="bus"> 
+            </div>
+            <div class="div2">
+                <img src="../image/car.png" alt="car" id="car"> 
+            </div>
+            <div class="div3"> 
+            <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            <a href="http://www.gps.ie/">vehicle gps</a></iframe>
+            </div>
+    </div>
 
 </body>
 
