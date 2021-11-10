3088fc1c0e931b2b7564a1cb0e8b56fa0146a893
athenaisT <athenais.thiburce@gmail.com>
2021-11-10 09:13:26 +0100

    bdd

--- public_html/index.php
+++ public_html/index.php
@@ -254,11 +254,7 @@ function formationroutes($fragments) {
       case "myformations":{
         call_user_func_array(["FormationControlleur","mesformations"], $fragments);
         break;
-      }
-      case "quizz":{
-        call_user_func_array(["FormationControlleur","displayQuizzformations"], $fragments);
-        break;
-      }
+      }    
       default :{
         header('Location: /connexion/404');
         break;
